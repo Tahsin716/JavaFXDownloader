e7bf47d8dbffa509fdd93cf319360ac85ad8f216
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-16 16:55:50 +0600

    Dowloader class update-1

--- src/downloader/Main.java
+++ src/downloader/Main.java
@@ -1,12 +1,42 @@
 package downloader;
 
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+
+    private class Donwloader extends Task<Void> {
+
+        //URL of the file to be downloaded
+        private String url;
+
+        //Constructor of the Downloader class that receives url as an argument
+        public Downloader(String url) {
+            this.url = url;
+        }
+
+
+        /**
+         * call method is overriden which:
+         * Receives the extension of the file to be downloaded
+         * Opens the url connection to the file link
+         * Determines the length of the file
+         * Reads the file and writes it in the directory provided
+         * Updates the progressbar with respect to the size of bytes downloaded
+         *
+         * @return
+         * @throws Exception
+         */
+        @Override
+        protected Void call() throws Exception {
+            return null;
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
